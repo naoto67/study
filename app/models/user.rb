cae57807ef7536137f405c1fc54b08fd15590f8c
naoto67 <uver.com67@gmail.com>
2018-12-19 16:56:27 +0900

    add auth token column to users. add token setting.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # users colomn validates
   validates :name, presence: true
   validates :screen_name, presence: true, uniqueness: true
+
+  # password setting
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # auth_token setting
+  has_secure_token :auth_token
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_050737) do
+ActiveRecord::Schema.define(version: 2018_12_19_075301) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_12_19_050737) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
     t.index ["screen_name"], name: "index_users_on_screen_name", unique: true
   end
 
