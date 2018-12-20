2e746c49105db0284c52e2fa9e5ce4623ce99438
naoto67 <uver.com67@gmail.com>
2018-12-20 15:00:39 +0900

    remove password digest column from response data.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(screen_name: params[:screen_name])
     render(json: { status: 400, message: 'not found' }) && return unless user
     if user.login(params[:password])
-      render json: { status: 200, data: user }
+      render json: { status: 200, data: user.attributes.except('password_digest') }
     else
       render json: { status: 400, message: 'password is incorrect.' }
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i[update destroy]
   def index
-    users = User.all
+    users = User.pluck(:id, :name, :screen_name, :created_at)
     render json: { status: 200, data: users }
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      render json: { status: 201, data: { auth_token: user.auth_token } }
+      render json: { status: 201, data: { auth_token: user.attributes.except('password_digest') } }
     else
       render json: { status: 400, message: 'bad request.' }
     end
