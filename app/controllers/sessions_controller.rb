082e9751c2a439077ec049aab9d3892b2f5695be
naoto67 <uver.com67@gmail.com>
2018-12-20 11:50:06 +0900

    add comment to create action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  # find user by screen name.
+  # it is authorize found user by password
+  # if password is incorrect, return 400.
+  # if password is correct, return 200 and user's auth token.
   def create
     user = User.find_by(screen_name: params[:screen_name])
     render(json: { status: 400, message: 'not found' }) && return unless user
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index create update destroy]
-  post '/users/login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
 end
